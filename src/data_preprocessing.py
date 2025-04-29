1c4b6ccec906da7e4a3a3306ec737456bf967ac5
Jnchuco <joanachuco@gmail.com>
2025-04-29 21:02:05 +0100

    Refactor dataset configuration and update TACODataset for improved data handling; enhance test cases for data loaders

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -26,7 +26,10 @@ class TACODataset(Dataset):
             A.Blur(blur_limit=3, p=0.1)
         ], bbox_params=A.BboxParams(
             format='coco', 
-            label_fields=['labels']))
+            label_fields=['labels'],
+            min_area=1,
+            min_visibility=0.1,
+        ))
 
     def __len__(self):
         return len(self.image_ids)
@@ -38,8 +41,7 @@ class TACODataset(Dataset):
 
         image = cv2.imread(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        original_height, original_width = image.shape[:2]
-
+        height, width = image.shape[:2]
 
         ann_ids = self.coco.getAnnIds(imgIds=img_id)
         annotations = self.coco.loadAnns(ann_ids)
@@ -49,15 +51,20 @@ class TACODataset(Dataset):
 
         for ann in annotations:
             x, y, w, h = ann['bbox']
-            x_max = min(x + w, original_width)
-            y_max = min(y + h, original_height)
-            w = x_max - x
-            h = y_max - y
+            
+            x = max(0, float(x))
+            y = max(0, float(y))
+            w = min(float(w), width - x)
+            h = min(float(h), height - y)
             
             if w > 0 and h > 0:
-                boxes.append([x, y, w, h])
+                boxes.append([
+                    max(0, x) / width,
+                    max(0, y) / height,
+                    min(width, x + w) / width,
+                    min(height, y + h) / height
+                ])
                 labels.append(ann['category_id'])
-
 
         if self.augment and len(boxes) > 0:
             augmented = self.augmentation(
@@ -66,43 +73,30 @@ class TACODataset(Dataset):
                 labels=labels
             )
         
-            
-            valid_boxes = []
-            valid_labels = []
             augmented_image = augmented['image']
-            aug_height, aug_width = augmented_image.shape[:2]
-            
-            for box, label in zip(augmented['bboxes'], augmented['labels']):
-                x, y, w, h = box
-                
-                x = max(0, min(x, aug_width - 1))
-                y = max(0, min(y, aug_height - 1))
-                w = max(1, min(w, aug_width - x))
-                h = max(1, min(h, aug_height - y))
-                valid_boxes.append([x, y, w, h])
-                valid_labels.append(label)
-            
             image = augmented_image
-            boxes = valid_boxes
-            labels = valid_labels
+            
+            boxes = []
+            for box in augmented['bboxes']:
+                x_min, y_min, x_max, y_max = box
+                boxes.append([
+                    x_min * width,
+                    y_min * height,
+                    x_max * width,
+                    y_max * height
+                ])
 
-        formatted_boxes = []
-        for box in boxes:
-            x, y, w, h = box
-            formatted_boxes.append([x, y, x + w, y + h])
-
+        if self.transform:
+            transformed = self.transform(image=image)
+            image = transformed['image']
 
         target = {
-            'boxes': torch.as_tensor(formatted_boxes, dtype=torch.float32),
+            'boxes': torch.as_tensor(boxes, dtype=torch.float32),
             'labels': torch.as_tensor(labels, dtype=torch.int64),
             'image_id': torch.as_tensor([img_id]),
         }
 
-        if self.transform:
-            image = self.transform(image=image)
-
         return image, target
-
 
 def create_data_loaders(config_path):
     with open(config_path) as f:
@@ -114,29 +108,28 @@ def create_data_loaders(config_path):
     ])
     
     train_dataset = TACODataset(
-        root_dir=config['train_images'],
-        annotation_file=config['train_annotations'],
+        root_dir=config['train']['images'],
+        annotation_file=config['train']['annotations'],
         transform=transform,
         augment=True
     )
     
     val_dataset = TACODataset(
-        root_dir=config['val_images'],
-        annotation_file=config['val_annotations'],
+        root_dir=config['val']['images'],
+        annotation_file=config['val']['annotations'],
         transform=transform
     )
     
-    # Data loaders
     train_loader = DataLoader(
         train_dataset,
-        batch_size=config['batch_size'],
+        batch_size=config['train']['batch_size'],
         shuffle=True,
         collate_fn=lambda batch: tuple(zip(*batch))
     )
     
     val_loader = DataLoader(
         val_dataset,
-        batch_size=config['batch_size'],
+        batch_size=config['val']['batch_size'],
         collate_fn=lambda batch: tuple(zip(*batch))
     )
     
