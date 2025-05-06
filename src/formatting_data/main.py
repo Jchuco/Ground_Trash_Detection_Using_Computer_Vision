bd20391b3c49e3e3f7434081cd1c7b929247db06
Jnchuco <joanachuco@gmail.com>
2025-05-06 10:01:57 +0100

    Refactor main.py to remove unused directory creation code and clarify dataset path update

--- src/formatting_data/main.py
+++ src/formatting_data/main.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 from src.formatting_data.taco2yolo import taco2yolo
 from src.formatting_data.merge_classes import merge_classes_to_superclasses
@@ -12,6 +11,7 @@ config = get_config(config_path)
 root_dir = config['dataset_original']['root_dir']
 path_to_annotations = config['dataset_original']['annotation_file']
 original_images_dir = config['dataset_original']['images']
+new_root_dir = config['dataset']['root_dir']
 
 merge_classes_to_superclasses(
     input_json_path=f"{root_dir}/annotations.json",
@@ -20,21 +20,15 @@ merge_classes_to_superclasses(
     output_images_dir=original_images_dir
 )
 
-train_dir = config['train']['labels']
-val_dir = config['val']['labels']
-new_root_dir = config['dataset']['root_dir']
-
-for i_dir in [train_dir, val_dir]:
-    a = Path(i_dir)
-    a.mkdir(parents=True, exist_ok=True)
-
-
 taco2yolo(path_to_annotations, original_images_dir, f"{new_root_dir}/labels")
 
-
+# This code is to fix a bug.
+# The bug was that in the dataset.yaml file
+# created by the function taco2yolo, the path
+# was skipping the root directory.
 dataset = config['dataset']['dataset']
 with open(dataset, 'r') as file:
     data = yaml.safe_load(file)
-data['path'] = f"../{Path.cwd().parts[-1]}"  # current path
+data['path'] = f"../{Path.cwd().parts[-1]}"  # Root directory
 with open(dataset, 'w') as file:
     yaml.dump(data, file)
